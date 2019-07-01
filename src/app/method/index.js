54255eec71b077d97d316ad7fa2eb34f9e81627a
RoryLetteney <rory.letteney@gmail.com>
2019-06-30 19:43:58 -0500

    Refactored a bit to try and get the label background color to change upon input selection.

--- src/app/method/index.js
+++ src/app/method/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import './styles.scss';
 
-export default ({ type }) => {
+export default ({ uniqueId, type }) => {
   return (
-    <label>
-      <input type="radio" name="method" value={type.toLowerCase()} />
-      <span>{type}</span>
-    </label>
+    <div className="method">
+      <input id={uniqueId} type="radio" name="method" value={type.toLowerCase()} />
+      <label htmlFor={uniqueId}>{type}</label>
+    </div>
   )
 }
